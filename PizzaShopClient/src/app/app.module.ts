c61fb5eb7ac61f894ba45dc534f607670ccfb71f
Aleksandr Shaduro <a.shaduro@outlook.com>
2020-08-18 10:54:52 +0300

    + add signalR to UI client

--- PizzaShopClient/src/app/app.module.ts
+++ PizzaShopClient/src/app/app.module.ts
@@ -8,6 +8,25 @@ import { PizzaRoutingModule } from './pizzarouting.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { SimpleNotificationsModule } from 'angular2-notifications';
+import {
+  SignalRModule,
+  SignalRConfiguration,
+  ConnectionTransports,
+  ConnectionTransport,
+} from 'ng2-signalr';
+
+export function initConfig(): SignalRConfiguration {
+  const cfg = new SignalRConfiguration();
+
+  cfg.hubName = 'sample';
+  cfg.url = 'http://demovm:50698/';
+  cfg.transport = [
+    ConnectionTransports.webSockets,
+    ConnectionTransports.longPolling,
+  ];
+
+  return cfg;
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,9 +35,10 @@ import { SimpleNotificationsModule } from 'angular2-notifications';
     NoopAnimationsModule,
     SimpleNotificationsModule.forRoot(),
     CoreModule.forRoot(),
+    SignalRModule.forRoot(initConfig),
     PizzaRoutingModule,
   ],
-  providers: [,],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- PizzaShopClient/src/app/core/components/navbar/navbar.component.ts
+++ PizzaShopClient/src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
+import { PizzaDto } from './../../../pizza/services/pizza.service';
+import { NotificationsService } from 'angular2-notifications';
+import { SignalR, ISignalRConnection } from 'ng2-signalr';
 import { UserDto } from '../../model/userDto';
 import { LoginService } from '../../../core/services/login.service';
-import { NotificationService } from '../../services/notification.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -15,9 +17,14 @@ export class NavbarComponent implements OnInit, OnDestroy {
   email: string;
   user$: Observable<UserDto>;
   isLogged$: Observable<boolean>;
+  private connection: ISignalRConnection;
   // private subscription: Subscription;
 
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private hub: SignalR,
+    private ntf: NotificationsService
+  ) {}
 
   ngOnDestroy(): void {
     // this.subscription.unsubscribe();
@@ -26,10 +33,31 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.user$ = this.loginService.LoggedOn$;
     this.isLogged$ = this.loginService.isLoggedOn$;
+
+    this.hub
+      .connect()
+      .then((c) => {
+        this.connection = c;
+        this.connection
+          .listenFor<string>('UpdateMessage')
+          .subscribe((msg) => this.ntf.warn('Message', msg));
+        this.connection
+          .listenFor<PizzaDto>('PizzaAdded')
+          .subscribe((dto) => this.ntf.success('Pizza Added', dto));
+      })
+      .catch((reason) =>
+        console.error(`Cannot connect to hub sample ${reason}`)
+      );
     // this.subscription = this.loginService.LoggedOn$.subscribe();
   }
 
   logout() {
     this.loginService.logout();
+  }
+
+  sendMessage(msg: string) {
+    if (this.connection) {
+      this.connection.invoke('SendMessage', msg);
+    }
   }
 }
